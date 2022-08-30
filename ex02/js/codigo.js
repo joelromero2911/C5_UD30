d79f9a89cd63c75727e81dc449723dbb19df63cd
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-08-30 10:23:38 +0200

    ex02

---
+++ ex02/js/codigo.js
@@ -0,0 +1,9 @@
+// Variable que contiene la cadena de texto que se mostrara en el pop up
+let mensaje = "Hola Mundo!\nQué fácil es inclui 'comillas simples'\ny \"comillas dobles\"";
+
+// Pop up de alerta al iniciar la pagina
+alert(mensaje);
+
+// Muestra por la consola del navegador las siguientes linias de texto:
+console.log("Hola mundo!");
+console.log("Soy el primer script");
