ff014c140243b2408ee0320bdfb7f0ea63170c9e
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-08-30 10:28:43 +0200

    ex03

---
+++ ex03/js/codigo.js
@@ -0,0 +1,7 @@
+// Array que contiene los meses del año.
+let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre"];
+
+// Loop que printa todos los objetos del array 'meses'.
+meses.forEach(mes => {
+    console.log(mes);
+});
