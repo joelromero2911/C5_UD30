13f2e91f7146ecb984d2eab2b04e333ebe33b475
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-08-30 10:16:06 +0200

    ex01

---
+++ ex01/js/codigo.js
@@ -0,0 +1,6 @@
+// Pop up de alerta al iniciar la pagina
+alert("Recuerda tener activado javascript!");
+
+// Muestra por la consola del navegador las siguientes linias de texto:
+console.log("Hola mundo!");
+console.log("Soy el primer script");
