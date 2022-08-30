742edfff29f2772dd4e5fe6183c97aec1eb89c53
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-08-30 11:28:32 +0200

    ex06, ex07

--- ex07/js/codigo.js
+++ ex07/js/codigo.js
@@ -1,18 +1,9 @@
-//Array con letras
-let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
-let numero = prompt("Introduce el DNI del que quieres calcular la letra", "32452345");
-let letra = prompt("Introduce la letra que crees que es la correcta", "N");
-letra = letra.toUpperCase();
-let letraFinal = letras[numero % 23];
+let num = prompt("Introduce el numero del que quieres calcular el factorial:");
 
-if(numero < 0 || numero > 99999999) {
-  alert("El número proporcionado no es válido");
+let factorial = num;
+
+for (let i = num-1; i > 0; i--) {
+    factorial *= i  
 }
-else {
-  if(letraFinal != letra) {
-    alert("No has acertado!\nLa letra para el DNI: " + numero + " es la \"" + letraFinal + "\"");
-  }
-  else {
-    alert("Has acertado!");
-  }
-}
+
+alert("Resultado: " + factorial);
